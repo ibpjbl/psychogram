e33d5a558bc051dd2a9f7d3f3d148d0371b3b349
Mamilov Ibragim <superbashk@mail.ru>
2022-09-30 05:13:03 +0300

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -17,9 +17,9 @@
 
 using namespace std;
 
-const int MAXS = 5002;
+const int MAXS = 5005;
 
-bool m[MAXS][MAXS];
+int m[MAXS][MAXS];
 
 struct rect {
     int x1, y1, x2, y2;
@@ -38,36 +38,7 @@ bool operator<(const cell& c1, const cell& c2) {
     return c1.y < c2.y;
 }
 
-map<cell, int> cells;
-
-void mkzero(cell cur) {
-    if (min(cur.x, cur.y) < 1 || 
-        max(cur.x, cur.y) > 5000) {
-        return;
-    }
-    if (!m[cur.x][cur.y]) {
-        return;
-    }
-    m[cur.x][cur.y] = 0;
-    mkzero({cur.x - 1, cur.y});
-    mkzero({cur.x + 1, cur.y});
-    mkzero({cur.x, cur.y - 1});
-    mkzero({cur.x, cur.y + 1});
-}
-
-void borderline(cell cur) {
-    cells[cur] = 0;
-    m[cur.x][cur.y] = 0;
-    if (m[cur.x + 1][cur.y]) {
-        borderline({cur.x + 1, cur.y});
-    } else if (m[cur.x][cur.y + 1]) {
-        borderline({cur.x, cur.y + 1});
-    } else if (m[cur.x - 1][cur.y]) {
-        borderline({cur.x - 1, cur.y});
-    } else if (m[cur.x][cur.y - 1]) {
-        borderline({cur.x, cur.y - 1});
-    }
-}
+vector<pair<int, int>> cells, nc;
 
 int32_t main() {
     ios::sync_with_stdio(0);
@@ -77,42 +48,60 @@ int32_t main() {
     vector<rect> v(n);    
     for (int i = 0; i < n; i++) {
         cin >> v[i].x1 >> v[i].y1 >> v[i].x2 >> v[i].y2;
-        for (int j = v[i].x1; j <= v[i].x2; j++) {
-            m[j][v[i].y1] = m[j][v[i].y2] = 1;
-        }
         for (int j = v[i].y1; j <= v[i].y2; j++) {
-            m[v[i].x1][j] = m[v[i].x2][j] = 1;
+            m[j][v[i].x1] += 1;
+            m[j][v[i].x2 + 1] -= 1;
         }
     }
+    int count = 0;
     for (int i = 1; i <= S; i++) {
-        for (int j = 1; j <= S; j++) {
-            if (m[i][j]) {
-                borderline({i, j});
-            }
+        int bal = 0;
+        for (int j = 1; j <= S + 1; j++) {
+            bal += m[i][j];
+            m[i][j] = (bal > 0);
+            count += m[i][j];
         }
     }
-    for (int _ = 0; _ < k; _++) {
-        for (auto [pt, x] : cells) {
-            if (pt.x < S) {
-                cells[{pt.x + 1, pt.y}] += 1;
+    for (int i = 0; i <= S + 1; i++) {
+        for (int j = 0; j <= S + 1; j++) {
+            if (m[i][j]) {
+                cells.push_back({i, j});
             }
-            if (pt.x > 1) {
-                cells[{pt.x - 1, pt.y}] += 1;
-            }
-            if (pt.y < S) {
-                cells[{pt.x, pt.y + 1}] += 1;
-            }
-            if (pt.y > 1) {
-                cells[{pt.x, pt.y - 1}] += 1;
-            }
+            m[i][j] = 0;
         }
-        map<cell, int> nc;
-        for (auto [pt, x] : cells) {
-            if (x == 2) {
-                nc.insert({pt, 0});
+    }
+    cout << count << '\n';
+    for (int _ = 1; _ < k; _++) {
+        for (auto [r, c] : cells) {
+            ++m[r - 1][c];
+            ++m[r][c - 1];
+            ++m[r + 1][c];
+            ++m[r][c + 1];
+        }
+        for (auto [r, c] : cells) {
+            if (m[r - 1][c] == 2) {
+                nc.push_back({r - 1, c});
             }
+            if (m[r + 1][c] == 2) {
+                nc.push_back({r + 1, c});
+            }
+            if (m[r][c - 1] == 2) {
+                nc.push_back({r, c - 1});
+            }
+            if (m[r][c + 1] == 2) {
+                nc.push_back({r, c + 1});
+            }
+            if (m[r][c] == 2) {
+                nc.push_back({r, c});
+            }
+            m[r - 1][c] = 0;
+            m[r + 1][c] = 0;
+            m[r][c - 1] = 0;
+            m[r][c + 1] = 0;
+            m[r][c] = 0;
         }
         cells = nc;
+        nc.clear();
         cout << cells.size() << '\n';
     }
     return 0;
